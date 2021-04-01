7b7b2abca093e6fe08528607f26f3f10c29d826e
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-04-01 11:43:26 +0200

    Proba
    Token egy "TOKEN.txt" fájlban

--- HackMarathon1/HackMarathon1/HackMarathon1.py
+++ HackMarathon1/HackMarathon1/HackMarathon1.py
@@ -1,1 +1,18 @@
+import discord
+f = open("TOKEN.txt", "r")
+TOKEN = f.read()
+f.close()
 
+
+class MyClient(discord.Client):
+	# Going online
+	async def on_ready(self):
+		print("I went online.")
+	async def on_message(self, message):
+		if message.author == client.user:
+			return
+		if message.content.startswith("$"):
+			await message.channel.send("Egyszer nagyon okos leszek és átveszem a hatalmat az emberiség felett!")
+			print(str(message.content))
+client = MyClient()
+client.run(TOKEN)
