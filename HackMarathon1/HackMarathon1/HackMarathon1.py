0c1fd926427dc0ef106fa22fccd79e2073908c89
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-04-04 14:39:28 +0200

    Reaction Event

--- HackMarathon1/HackMarathon1/HackMarathon1.py
+++ HackMarathon1/HackMarathon1/HackMarathon1.py
@@ -3,6 +3,8 @@ f = open("TOKEN.txt", "r")
 TOKEN = f.read()
 f.close()
 
+# Bot communication
+choose_food_type = "Choose which kind of food you want to eat!"
 
 class MyClient(discord.Client):
 	# Going online
@@ -10,9 +12,21 @@ class MyClient(discord.Client):
 		print("I went online.")
 	async def on_message(self, message):
 		if message.author == client.user:
-			return
-		if message.content.startswith("$"):
-			await message.channel.send("Egyszer nagyon okos leszek és átveszem a hatalmat az emberiség felett!")
+			if message.content == choose_food_type:
+				#await message.add_reaction("🥩") # cut of meat
+				await message.add_reaction("\N{cut of meat}")
+				await message.add_reaction("🍕")
+				await message.add_reaction("\N{hamburger}")
+			else:
+				pass
+		if message.content.startswith("$order"):
 			print(str(message.content))
+			# First step: What kind of food do you want to eat?
+			await message.channel.send(choose_food_type)
+	async def on_reaction_add(self, reaction, user):
+		if user == client.user:
+			return
+		await reaction.message.channel.send(str(user) + " chose " + str(reaction.emoji))
+
 client = MyClient()
 client.run(TOKEN)
